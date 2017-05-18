f786c62e9567b2e9b1d3a9ee861c0321832a871e
Mr.Cthulnull <cthulnull@gmail.com>
2017-05-18 18:39:51 +0200

    Update euromillones.py

--- euromillones.py
+++ euromillones.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 from random import randint as aleatorio
-#Programa para rellenar boleto Euromillones
-#Elegir 5 numeros y 2 estrellas
+
+#Select 5 numbers and 2 stars.
 
 #Variables
 a = 1
 e = 1
-#5 numeros aleatorios
-print ("Los 5 numeros son:")
+# 5 random numbers
+print ("The 5 numbers are:")
 while (a < 6):
     b = aleatorio(1,50)
     a += 1
     print (b)
 
-# 2 estrellas
-print ("Las estrellas son:")
+# 2 random stars
+print ("The 2 stars are:")
 while (e < 3):
     h = aleatorio(1,11)
     e += 1
