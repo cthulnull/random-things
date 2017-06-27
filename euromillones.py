5548b158f43b80ec7d5a63ae456a42700f2923b3
Mr.Cthulnull <cthulnull@gmail.com>
2017-06-27 13:31:50 +0200

    Update euromillones.py

--- euromillones.py
+++ euromillones.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
+
 from random import randint as aleatorio
 
-#Select 5 numbers and 2 stars.
+#Funcion combinacion, obtiene los numeros aleatorios y los ordena.
+def combinacion(num_comb, num_eleg, repetir = False, ordenar = True):
+    elementos = []
+    aux = num_comb
+    while aux > 0:
+        numero = aleatorio(1,num_eleg)
+        if repetir:
+            elementos.append(numero)
+            aux = aux - 1
+        else:
+            if elementos.count(numero) == 0:
+                elementos.append(numero)
+                aux = aux - 1
+    if ordenar:
+        elementos.sort()
+    return elementos
 
-#Variables
-a = 1
-e = 1
-# 5 random numbers
-print ("The 5 numbers are:")
-while (a < 6):
-    b = aleatorio(1,50)
-    a += 1
-    print (b)
 
-# 2 random stars
-print ("The 2 stars are:")
-while (e < 3):
-    h = aleatorio(1,11)
-    e += 1
-    print (h)
+
+#Imprime los 5 numeros
+print "Los 5 numeros son:"
+print combinacion(5,50)
+
+#Imprime las 2 estrellas
+print "Las estrellas son:"
+print combinacion(2,11)
